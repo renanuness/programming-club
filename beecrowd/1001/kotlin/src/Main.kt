ef3ca6a995b3b7d225700019a99b9f833d172e4f
renan <renan1504@gmail.com>
2024-05-25 16:41:32 -0300

    1001 kotlin version

---
+++ beecrowd/1001/kotlin/src/Main.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`);
+    val a = sc.next();
+    val b = sc.next();
+
+    println("X = " + (a.toInt() + b.toInt()));
+}
