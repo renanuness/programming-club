9589ed73ba83b5e12ced6729b42a70082db22dac
renanuness <renan1504@gmail.com>
2024-08-14 09:28:40 -0300

    2251

--- beecrowd.sh
+++ beecrowd.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-cd /home/renan/Documents/engenharia-comp/programming-club/beecrowd/;
-mkdir $1;
-cd $1;
-mkdir $2;
-cd $2;
-
+create(){
+    cd /home/renan/Documents/engenharia-comp/programming-club/beecrowd/;
+    mkdir $1;
+    cd $1;
+    mkdir $2;
+    cd $2;
+}
